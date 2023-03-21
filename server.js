1f8c4c443ed6a65f1aa0e17ef2eb8fa124d0ba0a
DESKTOP-V62HKP3\Administrator <kimdubu07lia@gmail.com>
2023-03-21 03:51:47 +0100

    update 4

--- server.js
+++ server.js
@@ -3,20 +3,34 @@ const dotenv = require("dotenv");
 const logger = require("morgan");
 const cors = require("cors");
 require("colors");
+const corsOpts = {
+    origin: '*',
+
+    methods: [
+        'GET',
+        'POST',
+    ],
+
+    allowedHeaders: [
+        'Content-Type',
+    ],
+};
+
+app.use(cors(corsOpts));
 
 const db = require("./config/db");
 const swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('./swagger.json');
+    swaggerDocument = require('./swagger.json');
 
 const app = express();
 
 dotenv.config({ path: "./config/config.env" });
 
-if (process.env.NODE_ENV === "production") console.log = function() {};
+if (process.env.NODE_ENV === "production") console.log = function () { };
 
 if (process.env.NODE_ENV === "development") app.use(logger("dev"));
 
-app.use(cors());
+// app.use(cors());
 
 // DB Connection
 db(app);
